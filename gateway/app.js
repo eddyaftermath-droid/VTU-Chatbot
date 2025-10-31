11f052aae5a48cf9b366a95ba98f29518ee7330c
eddyaftermath-droid <manchiajr@gmail.com>
2025-10-31 02:38:16 -0500

    Set up Express app with webhook routes

--- gateway/app.js
+++ gateway/app.js
@@ -1,1 +1,18 @@
+/ gateway/app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
+const webhookRoutes = require('./routes/webhook');
+const paymentsWebhook = require('./routes/paymentsWebhook');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/webhook', webhookRoutes);
+app.use('/webhook/payments', paymentsWebhook);
+
+app.get('/', (req, res) => res.send('WhatsApp Chatbot Gateway (serverless)'));
+
+// export the app for Vercel
+module.exports = app;
